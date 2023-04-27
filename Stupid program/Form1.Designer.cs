56a3b069f25733993ea008dc6969e75a8753a669
Makskaii <maksimkorz14@mail.ru>
2023-04-27 22:55:15 +0300

    Добавьте файлы проекта.

---
+++ Stupid program/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Stupid_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bPredictor = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.bPredictor);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(654, 410);
+            this.splitContainer1.SplitterDistance = 377;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(5, 345);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(567, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Задай вопрос на который можно ответить да или нет";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bPredictor
+            // 
+            this.bPredictor.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.bPredictor.Font = new System.Drawing.Font("Bauhaus 93", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bPredictor.ForeColor = System.Drawing.Color.Black;
+            this.bPredictor.Location = new System.Drawing.Point(248, 130);
+            this.bPredictor.Name = "bPredictor";
+            this.bPredictor.Size = new System.Drawing.Size(156, 88);
+            this.bPredictor.TabIndex = 0;
+            this.bPredictor.Text = "Predict";
+            this.bPredictor.UseVisualStyleBackColor = false;
+            this.bPredictor.Click += new System.EventHandler(this.bRandom_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.ForeColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(650, 25);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(654, 410);
+            this.Controls.Add(this.splitContainer1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button bPredictor;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Stupid program/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Stupid_program
+{
+    public partial class Form1 : Form
+    {
+        private const string APP_NAME = "Random";
+        private readonly string PREDICTIONS_CONFIG_PATH = $"{Environment.CurrentDirectory}\\PredictionsConfig.json";
+        private string[] _predictions;
+        private Random _random = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = APP_NAME;
+
+            try
+            {
+                var data = File.ReadAllText(PREDICTIONS_CONFIG_PATH);
+                _predictions = JsonConvert.DeserializeObject<string[]>(data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (_predictions == null)
+                {
+                    Close();
+                }
+                else if (_predictions.Length == 0)
+                {
+                    MessageBox.Show("Я своё отработал, пойду отохну");
+                    Close();
+                }
+            }
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private async void bRandom_Click(object sender, EventArgs e)
+        {
+            bPredictor.Enabled = false;
+            await Task.Run(() => 
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        UpdateProgressBar(i);
+                        this.Text = $"{i}%";
+                    }));
+
+                    Thread.Sleep(20);
+                }
+            });
+
+            var index = _random.Next(0, _predictions.Length);
+
+            var predication = _predictions[index];
+
+            MessageBox.Show($"{predication}!");
+            
+            progressBar1.Value = 0;
+            this.Text = APP_NAME;
+            bPredictor.Enabled = true;
+        }
+        private void UpdateProgressBar(int i)
+        {
+            if (i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+            }
+            else
+            {
+                progressBar1.Value = i + 1;
+            }
+            progressBar1.Value = i;
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
